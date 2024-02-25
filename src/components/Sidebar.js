30d0ba03f66342eced698c7fa006ba8864fc95ed
Muna Nadhifa Ridwan <mnadhifa@prixa.ai>
2024-02-25 23:36:23 +0700

    update sidebar icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,22 +2,31 @@ import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
   HomeIcon,
   UsersIcon,
-  XMarkIcon,
+  ChatBubbleLeftIcon,
+  BellIcon,
+  WalletIcon,
+  Cog8ToothIcon,
+  ArrowRightStartOnRectangleIcon,
 } from "@heroicons/react/24/outline";
 
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
+  { name: "Messages", href: "#", icon: ChatBubbleLeftIcon, current: false },
+  { name: "Notification", href: "#", icon: BellIcon, current: false },
+  { name: "Person", href: "#", icon: UsersIcon, current: false },
+  { name: "Wallet", href: "#", icon: WalletIcon, current: false },
+];
+
+const endNavigation = [
+  { name: "Settings", href: "#", icon: Cog8ToothIcon, current: false },
+  {
+    name: "Log out",
+    href: "#",
+    icon: ArrowRightStartOnRectangleIcon,
+    current: false,
+  },
 ];
 
 function classNames(...classes) {
@@ -40,11 +49,34 @@ export default function Example() {
       <div>
         <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-50 lg:block lg:w-20 lg:overflow-y-auto lg:bg-gray-900 lg:pb-4  rounded-tl-lg">
           <div className="flex h-16 shrink-0 items-center justify-center">
-            <h1 className="text-white">S.</h1>
+            <p className="text-white text-2xl">S.</p>
           </div>
           <nav className="mt-8">
             <ul role="list" className="flex flex-col items-center space-y-1">
               {navigation.map((item) => (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-800 text-white"
+                        : "text-gray-400 hover:text-white hover:bg-gray-800",
+                      "group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold"
+                    )}
+                  >
+                    <item.icon
+                      className="h-6 w-6 shrink-0"
+                      aria-hidden="true"
+                    />
+                    <span className="sr-only">{item.name}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <nav className="mt-60">
+            <ul role="list" className="flex flex-col items-center space-y-1">
+              {endNavigation.map((item) => (
                 <li key={item.name}>
                   <a
                     href={item.href}
